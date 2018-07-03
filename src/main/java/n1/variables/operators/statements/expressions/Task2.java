e691ab07fa706fdf3c1573917e8855dd2ec265ab
shchurkobohdan <shchurkobohdan@MacBook-Pro-Shchurko.local>
2018-07-03 23:21:40 +0300

    fixed square equation

--- src/main/java/n1/variables/operators/statements/expressions/Task2.java
+++ src/main/java/n1/variables/operators/statements/expressions/Task2.java
@@ -11,15 +11,26 @@ public class Task2 {
         int b = scaner.nextInt();
         System.out.println("Plese enter value for c variable: ");
         int c = scaner.nextInt();
-        double x1, x2;
+        double x, x1, x2, dyscryminant = Math.pow(b, 2) - 4 * a * c;
 
-        x1 = ((-b + (Math.sqrt(Math.pow(b, 2) - 4 * a * c)))) / (2 * a);
-        x2 = ((-b - (Math.sqrt(Math.pow(b, 2) - 4 * a * c)))) / (2 * a);
+        if (dyscryminant > 0){
+            x1 = ((-b + (Math.sqrt(dyscryminant)))) / (2 * a);
+            x2 = ((-b - (Math.sqrt(dyscryminant)))) / (2 * a);
 
+            System.out.println("--------------------------------------------");
+            System.out.println(a + "x^2" + " + " + b+"x +" + c + " = 0" + "\n");
+            System.out.println("Square root1: " + x1);
+            System.out.println("Square root2: " + x2);
+        }else if (dyscryminant == 0){
+            x = -b / (2 * a);
 
-        System.out.println("--------------------------------------------");
-        System.out.println(a + "x^2" + " + " + b+"x +" + c + " = 0" + "\n");
-        System.out.println("Square root1: " + x1);
-        System.out.println("Square root2: " + x2);
+            System.out.println("--------------------------------------------");
+            System.out.println(a + "x^2" + " + " + b+"x +" + c + " = 0" + "\n");
+            System.out.println("Square root: " + x);
+        }else {
+
+            System.out.println("--------------------------------------------");
+            System.out.println("There is no valid square root.");
+        }
     }
 }
