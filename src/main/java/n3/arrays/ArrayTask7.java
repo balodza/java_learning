f645355e3a5c3c1f88ca8443241c2f9a8711565b
shchurkobohdan <shchurko.b@gmail.com>
2018-07-04 15:50:54 +0300

    n3.arrays/ArrayTask7 - removed extra condition

--- src/main/java/n3/arrays/ArrayTask7.java
+++ src/main/java/n3/arrays/ArrayTask7.java
@@ -12,7 +12,7 @@ public class ArrayTask7 {
         int count = 0;
 
         for (int i = 0; i < arr.length; i++){
-            if(arr[i] != 0 && arr[i]%7 == 0){
+            if(arr[i]%7 == 0){
                 sum+= arr[i];
                 count++;
                 System.out.print(arr[i] + " ");
