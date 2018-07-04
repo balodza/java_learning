097468b17bcecec2392cc40787fd90693d3bcfbb
shchurkobohdan <shchurko.b@gmail.com>
2018-07-04 16:44:46 +0300

    /n2/loops/Task4.java - removed extra variables 'counter'

--- src/main/java/n2/loops/Task4.java
+++ src/main/java/n2/loops/Task4.java
@@ -18,16 +18,15 @@ public class Task4 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int counter1 = 0;
-        int counter2 = 0;
-        int counter3 = 0;
+        int counter = 0;
+
         System.out.println("Enter a number: ");
         int number = scan.nextInt();
 
         System.out.println('\n'+"Solution 1");
         // solution 1
-        while (counter1 < number){
-            counter1++;
+        while (counter < number){
+            counter++;
             for (int j = 0; j < number; j++) {
                 System.out.print("*");
             }
@@ -37,23 +36,25 @@ public class Task4 {
         System.out.println("--------------"+'\n'+"Solution 2");
 
         //solution 2
+        counter = 0;
         do{
             for (int j = 0; j < number; j++) {
                 System.out.print("*");
             }
             System.out.println();
-            counter2++;
-        } while (counter2 < number);
+            counter++;
+        } while (counter < number);
 
         System.out.println("--------------"+'\n'+"Solution 3");
 
         //solution 3
+        counter = 0;
         while (true){
-            counter3++;
+            counter++;
             for (int k = 0; k < number; k++) {
                 System.out.print("*");
             }
-            if (counter3 < number){
+            if (counter < number){
                 System.out.println();
             }else {
                 break;
