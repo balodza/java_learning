84254d293e8dee7fab8b3c9f998056185e55a8f9
shchurkobohdan <shchurkobohdan@MacBook-Pro-Shchurko.local>
2018-07-10 22:39:16 +0300

    n3.arrays/ArrayTask7(8).java - changed the place of variable initialization

--- src/main/java/n3/arrays/ArrayTask7.java
+++ src/main/java/n3/arrays/ArrayTask7.java
@@ -7,7 +7,6 @@ package n3.arrays;
 public class ArrayTask7 {
     public static void main(String[] args) {
         int [] arr = { -5, 0, 1, 2, 3, 5, 28, 6, 8, 11, 56, 87, 54, 34, 21, 9, 80, 81, 90, -100};
-        int avg = 0;
         int sum = 0;
         int count = 0;
 
@@ -18,7 +17,7 @@ public class ArrayTask7 {
                 System.out.print(arr[i] + " ");
             }
         }
-        avg = sum / count;
+         int avg = sum / count;
         System.out.println("/7 = " + avg);
     }
 }
