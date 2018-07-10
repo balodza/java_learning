a6e76b184d19c49ea044757243dec39529e8e013
shchurkobohdan <shchurkobohdan@MacBook-Pro-Shchurko.local>
2018-07-10 22:44:42 +0300

    src/main/java/n3/arrays/ArrayTask16.java - fixed output of array

--- src/main/java/n3/arrays/ArrayTask16.java
+++ src/main/java/n3/arrays/ArrayTask16.java
@@ -9,14 +9,14 @@ import java.util.Arrays;
 public class ArrayTask16 {
     public static void main(String[] args) {
         int i, k;
-        int[][] arr = {{5, 6, 1, 3}, {3, 4, 2, 1}, {1, 2, 2, 2}};
+        //int[][] arr = {{5, 6, 1, 3}, {3, 4, 2, 1}, {1, 2, 2, 2}};
+        int[][] arr = {{5, 6, 1, 3}, {3, 4, 2, 1}, {1, 2, 2, 2}, {3, 4, 5, 6}};
 
         for (i = 0; i < arr.length; i++) {
             for (k = 0; k < arr.length; k++) {
                 System.out.print(arr[i][k] + " ");
             }
-
-            System.out.println(arr[i][k] + " ");
+            System.out.println();
         }
     }
 }
